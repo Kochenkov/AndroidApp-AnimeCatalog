ed6045dea157ac633114bbca5b1bc141541d085b
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-01-16 20:09:52 +0300

    customise FilmInfoActivity

--- app/src/main/java/com/vkochenkov/filmscatalog/FilmInfoActivity.kt
+++ app/src/main/java/com/vkochenkov/filmscatalog/FilmInfoActivity.kt
@@ -1,12 +1,50 @@
 package com.vkochenkov.filmscatalog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.vkochenkov.filmscatalog.model.Film
 
 class FilmInfoActivity : AppCompatActivity() {
+
+    companion object {
+        const val FILM = "FILM"
+    }
+
+    lateinit var imageView: ImageView
+    lateinit var toolbar: Toolbar
+    lateinit var descriptorView: TextView
+    lateinit var film: Film
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_info)
+
+        initFields()
+        setToolbar()
+        fillFieldsWithData()
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun initFields() {
+        imageView = findViewById(R.id.image_view)
+        toolbar = findViewById(R.id.toolbar_film_info)
+        descriptorView = findViewById(R.id.tv_description_film)
+        film = intent.extras?.get(FILM) as Film
+    }
+
+    private fun fillFieldsWithData() {
+        imageView.setImageResource(film.imageRes)
+        toolbar.title = getText(film.titleRes)
+        descriptorView.text = getText(film.descriptionRes)
     }
 }
--- app/src/main/java/com/vkochenkov/filmscatalog/FilmsListActivity.kt
+++ app/src/main/java/com/vkochenkov/filmscatalog/FilmsListActivity.kt
@@ -1,27 +1,48 @@
 package com.vkochenkov.filmscatalog
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.vkochenkov.filmscatalog.model.Film
 
-class FilmsListActivity : AppCompatActivity() {
+class FilmsListActivity : AppCompatActivity(), View.OnClickListener{
+
+    companion object {
+        const val FILM = "FILM"
+    }
 
     lateinit var imagesArr: Array<Int>
     lateinit var titlesArr: Array<Int>
-    lateinit var descriptionsArr: Array<Int>
-
+    lateinit var buttonsArr: Array<Int>
     lateinit var filmsArr: Array<Film>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_films_list)
 
-        createData()
+        createAndFillData()
+        setOnClickListeners()
     }
 
-    private fun createData() {
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.btn_details_1 -> openFilmActivity(filmsArr[0])
+            R.id.btn_details_2 -> openFilmActivity(filmsArr[1])
+            R.id.btn_details_3 -> openFilmActivity(filmsArr[2])
+        }
+    }
+
+    private fun setOnClickListeners() {
+        for (i in 0..2) {
+            findViewById<Button>(buttonsArr[i]).setOnClickListener(this)
+        }
+    }
+
+    private fun createAndFillData() {
         filmsArr = arrayOf(
             Film(R.string.film_title_1, R.string.film_description_1, R.drawable.im_film_1),
             Film(R.string.film_title_2, R.string.film_description_2, R.drawable.im_film_2),
@@ -30,6 +51,7 @@ class FilmsListActivity : AppCompatActivity() {
 
         titlesArr = arrayOf(R.id.tv_title_film_1, R.id.tv_title_film_2, R.id.tv_title_film_3)
         imagesArr = arrayOf(R.id.imv_film_1, R.id.imv_film_2, R.id.imv_film_3)
+        buttonsArr = arrayOf(R.id.btn_details_1, R.id.btn_details_2, R.id.btn_details_3)
 
         for (i in 0..2) {
             val textView = findViewById<TextView>(titlesArr[i])
@@ -38,5 +60,12 @@ class FilmsListActivity : AppCompatActivity() {
             val imageView = findViewById<ImageView>(imagesArr[i])
             imageView.setImageResource(filmsArr[i].imageRes)
         }
+    }
+
+    private fun openFilmActivity(film: Film) {
+        val intent = Intent(this, FilmInfoActivity::class.java).apply {
+            putExtra(FILM, film)
+        }
+        startActivity(intent)
     }
 }
