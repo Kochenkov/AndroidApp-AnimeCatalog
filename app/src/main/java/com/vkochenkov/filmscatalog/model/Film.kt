df66771160d3ba0838480c1f70ce4c2619b0f2e4
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-01-16 20:07:03 +0300

    add parcelable interface to Film class

--- app/src/main/java/com/vkochenkov/filmscatalog/model/Film.kt
+++ app/src/main/java/com/vkochenkov/filmscatalog/model/Film.kt
@@ -1,7 +1,11 @@
 package com.vkochenkov.filmscatalog.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Film(
     val titleRes: Int,
     val descriptionRes: Int,
     val imageRes: Int
-)
+): Parcelable
