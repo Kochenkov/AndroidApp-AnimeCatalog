679343e7b7c39ac0995fb406c04599aa127f3829
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-01-27 19:20:34 +0300

    added new films

--- app/src/main/java/com/vkochenkov/filmscatalog/activities/FilmsListActivity.kt
+++ app/src/main/java/com/vkochenkov/filmscatalog/activities/FilmsListActivity.kt
@@ -67,10 +67,9 @@ class FilmsListActivity : AppCompatActivity(), View.OnClickListener {
             Film(R.string.film_title_1, R.string.film_description_1, R.drawable.im_film_1),
             Film(R.string.film_title_2, R.string.film_description_2, R.drawable.im_film_2),
             Film(R.string.film_title_3, R.string.film_description_3, R.drawable.im_film_3),
-            Film(R.string.film_title_3, R.string.film_description_3, R.drawable.im_film_3),
-            Film(R.string.film_title_3, R.string.film_description_3, R.drawable.im_film_3),
-            Film(R.string.film_title_3, R.string.film_description_3, R.drawable.im_film_3)
-            //todo
+            Film(R.string.film_title_4, R.string.film_description_4, R.drawable.im_film_4),
+            Film(R.string.film_title_5, R.string.film_description_5, R.drawable.im_film_5),
+            Film(R.string.film_title_6, R.string.film_description_6, R.drawable.im_film_6)
         )
     }
 
