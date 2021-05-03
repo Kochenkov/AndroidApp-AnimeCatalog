c529f4f12472d190e5552c07fb2cba9e560c5c19
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-05-03 15:13:29 +0300

    fix database version

--- app/src/main/java/com/vkochenkov/filmscatalog/model/db/FilmsDatabase.kt
+++ app/src/main/java/com/vkochenkov/filmscatalog/model/db/FilmsDatabase.kt
@@ -3,7 +3,7 @@ package com.vkochenkov.filmscatalog.model.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [(Film::class)], version = 4)
+@Database(entities = [(Film::class)], version = 5)
 abstract class FilmsDatabase : RoomDatabase(){
 
     abstract fun filmsDao() : FilmsDao
