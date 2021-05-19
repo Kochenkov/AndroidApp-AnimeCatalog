69e6c2f0760f46aa259024a6c05e3eb193d92ad5
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-05-19 17:14:56 +0300

    first espresso test

--- app/src/androidTest/java/com/vkochenkov/filmscatalog/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vkochenkov/filmscatalog/ExampleInstrumentedTest.kt
@@ -1,24 +1,38 @@
 package com.vkochenkov.filmscatalog
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.vkochenkov.filmscatalog.presentation.activity.MainActivity
 
+import org.hamcrest.Matchers
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+//todo написать еще тестов
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.vkochenkov.filmscatalog", appContext.packageName)
+    fun test() {
+        //when
+        Espresso.onView(withId(R.id.menu_info_item)).perform(ViewActions.click())
+        Thread.sleep(5000)
+
+        //then
+        Espresso.onView(Matchers.allOf(withId(R.id.btn_share)))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
     }
 }
