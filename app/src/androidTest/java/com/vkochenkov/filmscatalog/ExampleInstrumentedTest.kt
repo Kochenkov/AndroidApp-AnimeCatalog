82ae12ad20705e39feb73c3f759b3fd286fe80bc
Vlad Kochenkov <kochenkov1993@gmail.com>
2021-05-20 22:07:42 +0300

    drop db state in espresso tests

--- app/src/androidTest/java/com/vkochenkov/filmscatalog/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vkochenkov/filmscatalog/ExampleInstrumentedTest.kt
@@ -1,17 +1,16 @@
 package com.vkochenkov.filmscatalog
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.vkochenkov.filmscatalog.presentation.activity.MainActivity
-
 import org.hamcrest.Matchers
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 //todo написать еще тестов
@@ -23,16 +22,25 @@ class ExampleInstrumentedTest {
     var activityRule: ActivityScenarioRule<MainActivity> =
         ActivityScenarioRule(MainActivity::class.java)
 
+    @Before
+    fun start() {
+        //дропаем базу перед тестами (с помощью оркестратора)
+        InstrumentationRegistry.getInstrumentation()
+            .uiAutomation
+            .executeShellCommand("pm clear com.vkochenkov.filmscatalog")
+            .close()
+    }
 
     @Test
     fun test() {
+
+        Thread.sleep(3000)
         //when
-        Espresso.onView(withId(R.id.menu_info_item)).perform(ViewActions.click())
-        Thread.sleep(5000)
+        onView(withId(R.id.item_film_like_btn)).perform(click())
+        onView(withId(R.id.menu_favourites_item)).perform(click())
 
         //then
-        Espresso.onView(Matchers.allOf(withId(R.id.btn_share)))
+        onView(Matchers.allOf(withId(R.id.item_favourite_film_delete_btn)))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
     }
 }
